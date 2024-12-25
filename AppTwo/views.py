081bcbfc7d6dd7a520982edcc6742ac49f760c55
Husayn <khaybabz@gmail.com>
2024-12-25 14:08:00 +0100

    Successfully added the authors url in apptwo

--- AppTwo/views.py
+++ AppTwo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+# CRUD operations for different models. Examples:
+# : Manage data related to "Authors" (fields: name, bio, date_of_birth, etc.).
 # Create your views here.
+
+def authors(request):
+  return HttpResponse("You can view the authours here.") 
