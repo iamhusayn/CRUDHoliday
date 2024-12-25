4029d740f57d125d148f37c7e027ff2e83cd7c13
Husayn <khaybabz@gmail.com>
2024-12-25 13:53:33 +0100

    Successfuly added home url welcome messsage

--- AppOne/views.py
+++ AppOne/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+# CRUD operations for different models. Examples:
+# : Manage data related to "Books" (fields: title, author, publication_date, etc.).
 # Create your views here.
+def welcome(request):
+    return HttpResponse("Hi, Welcome to Books Hub!")
